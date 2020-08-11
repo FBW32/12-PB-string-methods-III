23abbd6df95e06ca26b421cca8cef2a58ac1779c
kinan Naem <kinan.berlin@gmail.com>
2020-08-11 16:10:55 +0200

    Update 12-PB-string-methods-III.js
    complited
    
    Co-Authored-By: Ghassan Aldarwish <ghassanaldarwish@yahoo.com>
    Co-Authored-By: Mariam Fatima <mariamfatima.co@gmail.com>

--- Kinan/12-PB-string-methods-III.js
+++ Kinan/12-PB-string-methods-III.js
@@ -10,8 +10,8 @@ let char1=addchar.charAt(0);
 console.log(char1+addchar+char1);
 
 let var1="BoogieWoogie";
-let firstChar = var1.charAt(0); //B
-let lastChar = var1.substr(-1); //E
+let firstChar = var1.charAt(0); 
+let lastChar = var1.substr(-1); 
 console.log(lastChar+(var1.substring(1))+firstChar);
 
 let fox="the quick brown fox";
